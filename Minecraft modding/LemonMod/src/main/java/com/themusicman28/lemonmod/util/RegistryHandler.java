a1f0ad064a4fd992dacb385534fd4c2b3455cde1
Daniel J Crowley <dcrow2814@gmail.com>
2020-08-27 14:58:14 -0500

    Added in mod files

---
+++ Minecraft modding/LemonMod/src/main/java/com/themusicman28/lemonmod/util/RegistryHandler.java
@@ -0,0 +1,11 @@
+package com.themusicman28.lemonmod.util;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS);
+
+}
